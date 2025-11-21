e24ddffbed5f4030ed3220ee3c558c9011fb702a
Ahmad Danish <danish25299@gmail.com>
2025-11-21 23:12:19 +0900

    upgrade prisma

--- apps/backend/src/config/auth.ts
+++ apps/backend/src/config/auth.ts
@@ -1,8 +1,6 @@
-import { PrismaClient } from '@repo/database'
+import { prisma } from '@repo/database'
 import { betterAuth } from 'better-auth'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
-
-const prisma = new PrismaClient()
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
