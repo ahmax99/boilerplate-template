3d79a1bd0e9026f7456bfda51d349bb42741cb54
Ahmad Danish <danish25299@gmail.com>
2025-12-12 16:12:50 +0900

    refactor todos BE to match schema

--- apps/backend/src/modules/todos/domain/valueObjects/todoId.vo.ts
+++ apps/backend/src/modules/todos/domain/valueObjects/todoId.vo.ts
@@ -1,13 +1,13 @@
-import { randomInt } from 'node:crypto'
+import { v4 as uuid } from 'uuid'
 
 export class TodoId {
-  private readonly value: number
+  private readonly value: string
 
-  constructor(id?: number) {
-    this.value = id ?? randomInt(1, 1000000)
+  constructor(id?: string) {
+    this.value = id ?? uuid()
   }
 
-  getValue(): number {
+  getValue(): string {
     return this.value
   }
 
