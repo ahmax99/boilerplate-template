ddd8ff3d19958411403166592eb37a0a34000df5
Ahmad Danish <danish25299@gmail.com>
2026-07-12 00:58:56 +0900

    fix: fix resource changes on terraform plan
    Signed-off-by: Ahmad Danish <danish25299@gmail.com>

--- infra/terraform/modules/lambda-edge/versions.tf
+++ infra/terraform/modules/lambda-edge/versions.tf
@@ -6,10 +6,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.0"
-    }
     archive = {
       source  = "hashicorp/archive"
       version = "~> 2.0"
